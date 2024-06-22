d5ced7db4e44e53c8bb747010104910d5e107d3c
Aless4ndr0 <jiwemoyo@gmail.com>
2024-06-22 17:00:14 -0500

    funciona pero cuando le actualizo se borran los likes

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,35 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RecipeService } from '../../services/recipe.service';
 import { LocalStorageService } from '../../services/local-storage.service';
-import { CommentService } from '../../services/comment.service';
-import { LikeService } from '../../services/like.service';
 
 @Component({
   selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html'
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
   recipes: any[] = [];
   recipeForm: FormGroup;
-  token: string;
+  token: string | null;
+  isEditing: boolean = false;
+  currentRecipeId: string | null = null;
 
   constructor(
-    private fb: FormBuilder,
     private recipeService: RecipeService,
     private localStorageService: LocalStorageService,
-    private commentService: CommentService,
-    private likeService: LikeService
+    private formBuilder: FormBuilder
   ) {
-    this.recipeForm = this.fb.group({
-      title: [''],
-      description: [''],
-      ingredients: [''],
-      steps: [''],
+    this.recipeForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      ingredients: ['', Validators.required],
+      steps: ['', Validators.required],
       image: [null]
     });
 
-    this.token = this.localStorageService.getItem('token') ?? '';
+    this.token = this.localStorageService.getItem('token');
   }
 
   ngOnInit(): void {
@@ -39,70 +38,83 @@ export class UserProfileComponent implements OnInit {
   loadUserRecipes(): void {
     this.recipeService.getRecipesByUser().subscribe(
       recipes => {
-        this.recipes = recipes;
-        this.loadCommentsAndLikesForRecipes();
+        this.recipes = recipes.map((recipe: { likes: string | any[]; comments: any; }) => {
+          return {
+            ...recipe,
+            likesCount: recipe.likes.length,  // Contar likes
+            comments: recipe.comments         // Asegurar comentarios
+          };
+        });
       },
       error => console.error(error)
     );
-  }
-
-  private loadCommentsAndLikesForRecipes(): void {
-    this.recipes.forEach(recipe => {
-      this.commentService.getCommentsByRecipe(recipe._id).subscribe(
-        comments => {
-          recipe.comments = comments;
-        },
-        error => console.error(error)
-      );
-
-      this.likeService.getLikesCountByRecipe(recipe._id).subscribe(
-        data => {
-          recipe.likesCount = data.count;
-        },
-        error => console.error(error)
-      );
-    });
   }
 
   createRecipe(): void {
     const formData = new FormData();
     Object.keys(this.recipeForm.controls).forEach(key => {
-      formData.append(key, this.recipeForm.get(key)!.value);
+      const control = this.recipeForm.get(key);
+      if (control) {
+        formData.append(key, control.value);
+      }
     });
 
-    this.recipeService.createRecipe(formData, this.token).subscribe(
-      recipe => {
-        this.recipes.push(recipe);
-        this.recipeForm.reset();
-      },
-      error => console.error(error)
-    );
+    if (this.token) {
+      this.recipeService.createRecipe(formData, this.token).subscribe(
+        recipe => {
+          this.recipes.push(recipe);
+          this.recipeForm.reset();
+        },
+        error => console.error(error)
+      );
+    }
   }
 
-  updateRecipe(id: string): void {
+  editRecipe(recipe: any): void {
+    this.recipeForm.patchValue({
+      title: recipe.title,
+      description: recipe.description,
+      ingredients: recipe.ingredients.join(', '),
+      steps: recipe.steps
+    });
+    this.isEditing = true;
+    this.currentRecipeId = recipe._id;
+  }
+
+  updateRecipe(): void {
+    if (!this.currentRecipeId) return;
+
     const formData = new FormData();
     Object.keys(this.recipeForm.controls).forEach(key => {
-      formData.append(key, this.recipeForm.get(key)!.value);
+      const control = this.recipeForm.get(key);
+      if (control) {
+        formData.append(key, control.value);
+      }
     });
 
-    this.recipeService.updateRecipe(id, formData, this.token).subscribe(
-      updatedRecipe => {
-        const index = this.recipes.findIndex(recipe => recipe._id === id);
-        if (index !== -1) {
-          this.recipes[index] = updatedRecipe;
-        }
-      },
-      error => console.error(error)
-    );
+    if (this.token) {
+      this.recipeService.updateRecipe(this.currentRecipeId, formData, this.token).subscribe(
+        updatedRecipe => {
+          const index = this.recipes.findIndex(recipe => recipe._id === this.currentRecipeId);
+          if (index !== -1) {
+            this.recipes[index] = updatedRecipe;
+          }
+          this.isEditing = false;
+          this.currentRecipeId = null;
+          this.recipeForm.reset();
+        },
+        error => console.error(error)
+      );
+    }
   }
 
   deleteRecipe(id: string): void {
-    this.recipeService.deleteRecipe(id, this.token).subscribe(
-      () => {
-        this.recipes = this.recipes.filter(recipe => recipe._id !== id);
-      },
-      error => console.error(error)
-    );
+    if (this.token) {
+      this.recipeService.deleteRecipe(id, this.token).subscribe(
+        () => this.recipes = this.recipes.filter(recipe => recipe._id !== id),
+        error => console.error(error)
+      );
+    }
   }
 
   onFileChange(event: any): void {
