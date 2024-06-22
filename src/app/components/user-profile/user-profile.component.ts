174feea27db5ebcd0d613bd4ac97b0e137fe365a
Aless4ndr0 <jiwemoyo@gmail.com>
2024-06-22 11:36:34 -0500

    comportamiento de likes

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
 import { CommentService } from '../../services/comment.service';
+import { LikeService } from '../../services/like.service';
 import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
@@ -14,6 +15,7 @@ export class UserProfileComponent implements OnInit {
   constructor(
     private recipeService: RecipeService,
     private commentService: CommentService,
+    private likeService: LikeService,
     private localStorageService: LocalStorageService
   ) { }
 
@@ -21,17 +23,25 @@ export class UserProfileComponent implements OnInit {
     this.recipeService.getRecipesByUser().subscribe(
       recipes => {
         this.recipes = recipes;
-        this.loadCommentsForRecipes();
+        this.loadCommentsAndLikesForRecipes();
       },
       error => console.error(error)
     );
   }
 
-  private loadCommentsForRecipes(): void {
+
+  private loadCommentsAndLikesForRecipes(): void {
     this.recipes.forEach(recipe => {
       this.commentService.getCommentsByRecipe(recipe._id).subscribe(
         comments => {
           recipe.comments = comments;
+        },
+        error => console.error(error)
+      );
+
+      this.likeService.getLikesCountByRecipe(recipe._id).subscribe(
+        data => {
+          recipe.likesCount = data.count;
         },
         error => console.error(error)
       );
--- src/app/services/like.service.ts
+++ src/app/services/like.service.ts
@@ -1,22 +1,40 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class LikeService {
+//   private apiUrl = 'http://localhost:3000/api/likes';
+
+//   constructor(private http: HttpClient) { }
+
+//   likeRecipe(recipeId: string, token: string): Observable<any> {
+//     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+//     return this.http.post(this.apiUrl, { recipeId }, { headers });
+//   }
+
+//   unlikeRecipe(recipeId: string, token: string): Observable<any> {
+//     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+//     return this.http.delete(this.apiUrl, { headers, body: { recipeId } });
+//   }
+// }
+
+// src/app/services/like.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LikeService {
-  private apiUrl = 'http://localhost:3000/api/likes';
+  private apiUrl = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) { }
 
-  likeRecipe(recipeId: string, token: string): Observable<any> {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.post(this.apiUrl, { recipeId }, { headers });
-  }
-
-  unlikeRecipe(recipeId: string, token: string): Observable<any> {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.delete(this.apiUrl, { headers, body: { recipeId } });
+  getLikesCountByRecipe(recipeId: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/likes/${recipeId}/count`);
   }
 }
