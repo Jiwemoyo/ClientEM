6f6af7f77e2a69615fa2044cbaa599fd5990c0b2
Aless4ndr0 <jiwemoyo@gmail.com>
2024-06-24 10:23:36 -0500

    Recetas en la pagina principal

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+// src/app/components/recipe-list/recipe-list.component.ts
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: any[] = [];
 
+  constructor(private recipeService: RecipeService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.recipeService.getAllRecipes().subscribe((data: any[]) => {
+      this.recipes = data;
+    });
+  }
+
+  viewRecipe(id: string): void {
+    this.router.navigate(['/recipe', id]);
+  }
 }
