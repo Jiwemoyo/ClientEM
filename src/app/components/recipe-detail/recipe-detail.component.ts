c54280a15171b06e5ffddde0259023d80e77dd0a
Aless4ndr0 <jiwemoyo@gmail.com>
2024-06-25 10:08:07 -0500

    likes funcionales

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../../services/recipe.service';
 import { CommentService } from '../../services/comment.service';
+import { LikeService } from '../../services/like.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { jwtDecode } from 'jwt-decode';
@@ -12,16 +13,18 @@ import { jwtDecode } from 'jwt-decode';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: any;
+  recipe: any = {}; // Inicializar recipe como un objeto vacío
   commentForm: FormGroup;
   editCommentForm: FormGroup;
   editingCommentId: string | null = null;
   userId: string | null = null;
+  userHasLiked: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService,
     private commentService: CommentService,
+    private likeService: LikeService,
     private fb: FormBuilder,
     private localStorageService: LocalStorageService
   ) {
@@ -46,6 +49,43 @@ export class RecipeDetailComponent implements OnInit {
     if (id && token) {
       this.recipeService.getRecipeById(id, token).subscribe((data: any) => {
         this.recipe = data;
+        this.checkUserLike();
+      });
+    }
+  }
+
+  checkUserLike(): void {
+    if (this.recipe.likes && this.recipe.likes.some((like: any) => like.user === this.userId)) {
+      this.userHasLiked = true;
+    } else {
+      this.userHasLiked = false;
+    }
+  }
+
+  likeRecipe(): void {
+    const token = this.localStorageService.getItem('token');
+    if (token) {
+      this.likeService.likeRecipe(this.recipe._id, token).subscribe(() => {
+        this.userHasLiked = true;
+        if (this.recipe.likesCount !== undefined && this.recipe.likesCount !== null) {
+          this.recipe.likesCount += 1; // Incrementar el contador de likes
+        } else {
+          this.recipe.likesCount = 1; // Inicializar el contador de likes si es undefined o null
+        }
+      });
+    }
+  }
+
+  unlikeRecipe(): void {
+    const token = this.localStorageService.getItem('token');
+    if (token) {
+      this.likeService.unlikeRecipe(this.recipe._id, token).subscribe(() => {
+        this.userHasLiked = false;
+        if (this.recipe.likesCount !== undefined && this.recipe.likesCount !== null) {
+          this.recipe.likesCount -= 1; // Decrementar el contador de likes
+        } else {
+          this.recipe.likesCount = 0; // Inicializar el contador de likes si es undefined o null
+        }
       });
     }
   }
