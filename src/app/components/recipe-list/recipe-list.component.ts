a3a071d41e90aa016b03891381fb9481ec7f93ef
Aless4ndr0 <jiwemoyo@gmail.com>
2024-07-08 20:30:16 -0500

    agregue el buscador de recetas

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -10,16 +10,25 @@ import { RecipeService } from '../../services/recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: any[] = [];
+  filteredRecipes: any[] = [];
 
   constructor(private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit(): void {
     this.recipeService.getAllRecipes().subscribe((data: any[]) => {
       this.recipes = data;
+      this.filteredRecipes = data; // Inicialmente, todas las recetas están en la lista filtrada
     });
   }
 
   viewRecipe(recipeId: string): void {
     this.router.navigate(['/recipe', recipeId]);
+  }
+
+  onSearch(event: Event): void {
+    const searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredRecipes = this.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(searchTerm)
+    );
   }
 }
