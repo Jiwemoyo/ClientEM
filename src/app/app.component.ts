adab7d2b4236d305212f4a77875c3f56ffa0a2ed
Aless4ndr0 <jiwemoyo@gmail.com>
2024-07-25 13:58:36 -0500

    pantalla de carga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { LoadingService } from './services/loading.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'clientEasyMenu';
+export class AppComponent implements OnInit {
+  loading$ = this.loadingService.isLoading$;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private loadingService: LoadingService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Mostrar loading al iniciar la aplicación
+      this.loadingService.show();
+
+      // Ocultar loading cuando la página haya cargado completamente
+      window.addEventListener('load', () => {
+        this.loadingService.hide();
+      });
+
+      // Mostrar loading cuando se inicia una recarga de página
+      window.addEventListener('beforeunload', () => {
+        this.loadingService.show();
+      });
+    }
+  }
 }
