2e0858d817676f1794ba8df9c1f53abfddc2e466
Aless4ndr0 <jiwemoyo@gmail.com>
2024-07-25 14:14:54 -0500

    validaciones de correos

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -12,20 +12,30 @@ export class RegisterComponent {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   constructor(private authService: AuthService, private router: Router) { }
 
   register() {
+    if (!this.validateEmail()) {
+      this.errorMessage = "Por favor, ingresa un correo electrónico válido.";
+      return;
+    }
+
     const userData = {
       username: this.username,
       email: this.email,
       password: this.password
     };
 
-    
     this.authService.register(userData).subscribe(
       () => this.router.navigate(['/login']),
       error => this.errorMessage = error.error.message
     );
+  }
+
+  validateEmail(): boolean {
+    const regex = new RegExp(this.emailPattern);
+    return regex.test(this.email);
   }
 }
